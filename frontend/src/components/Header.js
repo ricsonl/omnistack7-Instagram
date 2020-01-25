3775afdc4254b6f562fc8a633b01baf111ded308
ricsonl <ricsonluizv@gmail.com>
2020-01-25 01:01:57 -0300

    [add] feed css changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
+
 import logo from '../assets/logo.svg';
 import cam from '../assets/camera.svg';
 
@@ -8,8 +11,15 @@ export default function Header(){
     return(
         <header id="main-header">
             <div className="header-content">
-                <img src={logo} alt="InstaRocket" />
-                <img src={cam} alt="Enviar publicação" />
+
+                <Link to="/">
+                    <img src={logo} alt="InstaRocket" />
+                </Link>
+
+                <Link to="/new">
+                    <img src={cam} alt="Enviar publicação" />
+                </Link> 
+
             </div>
         </header>
     );
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
     render() {
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Ricson Luiz</span>
+                            <span className="place">belzonte</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+                    <img src="http://localhost:3333/files/tenor.jpg" alt=""/>
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        <strong>2 curtidas</strong>
+                        <p>
+                            vi um pato andando sozinho 
+                            <span>#tururu #turururu</span>
+                        </p>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Ricson Luiz</span>
+                            <span className="place">belzonte</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+                    
+                    <img src="http://localhost:3333/files/tenor.jpg" alt="" />
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+                        <strong>2 curtidas</strong>
+                        <p>
+                            vi um pato andando sozinho
+                            <span>#tururu #turururu</span>
+                        </p>
+                    </footer>
+                </article>
+
+            </section>
         );
     }
 }
