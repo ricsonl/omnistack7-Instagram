5fa43c5401417cf7bbe0be8a539f8d8be6dbe941
ricsonl <ricsonluizv@gmail.com>
2020-01-25 15:31:30 -0300

    [add] get post data from api

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 
@@ -8,57 +9,42 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount(){
+        const response = await api.get('posts');
+        this.setState({ feed: response.data });
+    }
+
     render() {
         return(
             <section id="post-list">
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Ricson Luiz</span>
-                            <span className="place">belzonte</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src="http://localhost:3333/files/tenor.jpg" alt=""/>
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
-                        <strong>2 curtidas</strong>
-                        <p>
-                            vi um pato andando sozinho 
-                            <span>#tururu #turururu</span>
-                        </p>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Ricson Luiz</span>
-                            <span className="place">belzonte</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    
-                    <img src="http://localhost:3333/files/tenor.jpg" alt="" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
-                        <strong>2 curtidas</strong>
-                        <p>
-                            vi um pato andando sozinho
-                            <span>#tururu #turururu</span>
-                        </p>
-                    </footer>
-                </article>
-
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                            <span>{post.author}</span>
+                            <span className="place">{post.place}</span>
+                            </div>
+                            <img src={more} alt="Mais" />
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+                            <strong>{post.likes} curtidas</strong>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))}
             </section>
         );
     }
